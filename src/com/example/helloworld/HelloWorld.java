36821c6d71867001f7841bf52208bb1957f4b8d3
Valeriy Osipov <info@vosipov.com>
2016-03-24 18:22:04 +0300

    Initial commit

---
+++ src/com/example/helloworld/HelloWorld.java
@@ -0,0 +1,10 @@
+package com.example.helloworld;
+
+/**
+ * Created by info_000 on 2/17/2016.
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
